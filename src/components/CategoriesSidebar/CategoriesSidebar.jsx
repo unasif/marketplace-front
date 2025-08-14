7fa7cbd6027076a50ad717959adc1f07f070b446
OleksandrKuzyshyn <kyzyshunoleksandr40@gmail.com>
2025-08-14 07:17:33 +0300

    три рівні категорій

--- src/components/CategoriesSidebar/CategoriesSidebar.jsx
+++ src/components/CategoriesSidebar/CategoriesSidebar.jsx
@@ -1,49 +1,12 @@
 import React from "react";
-import AccordionArrow from "../../assets/accordionArrow.svg";
-import SubCategories from "../SubCategories/SubCategories";
 import styles from "./CategoriesSidebar.module.scss";
-import useCategoriesById from "../../hooks/useCategoriesById";
-import { useCategoryContext } from "../../contexts/CategoryContext";
+import SubCategories from "../SubCategories/SubCategories";
 
 const CategoriesSidebar = ({ token }) => {
-  const categories = useCategoriesById(token, null);
-  const { openId, updateOpenId } = useCategoryContext();
-
   return (
     <nav className={styles.linksSidebar}>
       <p>Категорії</p>
-      <ul>
-        {categories.map((category) => (
-          <li className={styles.menuItem} key={category.id}>
-            <button
-              className={styles.menuHeader}
-              onClick={() => updateOpenId(category.id)}
-            >
-              {category.name}
-              <img
-                src={AccordionArrow}
-                alt="Accordion Arrow"
-                className={
-                  category.id === openId
-                    ? `${styles.accordionArrow} ${styles.active}`
-                    : styles.accordionArrow
-                }
-              />
-            </button>
-            <div
-              className={
-                category.id === openId ? styles.subMenuOpen : styles.subMenu
-              }
-            >
-              <SubCategories
-                className={styles.subMenuList}
-                token={token}
-                categoryId={category.id}
-              />
-            </div>
-          </li>
-        ))}
-      </ul>
+      <SubCategories token={token} categoryId={null} />
     </nav>
   );
 };
--- src/components/SubCategories/SubCategories.jsx
+++ src/components/SubCategories/SubCategories.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useCategoriesById from "../../hooks/useCategoriesById";
+import AccordionArrow from "../../assets/accordionArrow.svg";
+import styles from "../CategoriesSidebar/CategoriesSidebar.module.scss";
 
-const SubCategories = ({ token, categoryId }) => {
+const SubCategories = ({ token, categoryId, level = 0 }) => {
   const categories = useCategoriesById(token, categoryId);
+  const [openIds, setOpenIds] = useState([]);
   const navigate = useNavigate();
 
   if (!categories || categories.length === 0) {
     return null;
   }
+
+  const toggleOpen = (id) => {
+    setOpenIds((prev) =>
+      prev.includes(id) ? prev.filter((openId) => openId !== id) : [...prev, id]
+    );
+  };
 
   const handleNavigation = (event, id) => {
     event.preventDefault();
@@ -16,15 +25,34 @@ const SubCategories = ({ token, categoryId }) => {
   };
 
   return (
-    <ul>
+    <ul className={styles.subMenuList} style={{ paddingLeft: level * 16 }}>
       {categories.map((category) => (
-        <li key={category.id}>
-          <Link
-            to={`/category/${category.id}`}
-            onClick={(e) => handleNavigation(e, category.id)}
-          >
-            {category.name}
-          </Link>
+        <li key={category.id} className={styles.menuItem}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+            <button
+              className={styles.menuHeader}
+              onClick={() => toggleOpen(category.id)}
+              style={{ marginLeft: 8 }}
+            >
+              <img
+                src={AccordionArrow}
+                alt="Accordion Arrow"
+                className={
+                  openIds.includes(category.id)
+                    ? `${styles.accordionArrow} ${styles.active}`
+                    : styles.accordionArrow
+                }
+              />
+            </button>
+          </div>
+          {openIds.includes(category.id) && (
+            <SubCategories
+              token={token}
+              categoryId={category.id}
+              level={level + 1}
+            />
+          )}
         </li>
       ))}
     </ul>
