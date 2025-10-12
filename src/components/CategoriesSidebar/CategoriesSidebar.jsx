d3ab0a5bc072a26c7b46ba921653a2a575b536ff
OleksandrKuzyshyn <kyzyshunoleksandr40@gmail.com>
2025-10-12 19:14:21 +0300

    Невелика доробка категорій

--- src/components/CategoriesSidebar/CategoriesSidebar.jsx
+++ src/components/CategoriesSidebar/CategoriesSidebar.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./CategoriesSidebar.module.scss";
 import SubCategories from "../SubCategories/SubCategories";
 
-const CategoriesSidebar = ({ token }) => {
+const CategoriesSidebar = ({ token, categories }) => {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path || location.pathname.startsWith(path);
+  };
+
   return (
     <nav className={styles.linksSidebar}>
       <p>Категорії</p>
       <SubCategories token={token} categoryId={null} />
+      <div className={styles.sidebar}>
+        {categories.map((cat) => {
+          const path = `/category/${cat.id}`;
+          return (
+            <Link
+              key={cat.id}
+              to={path}
+              className={`${styles.link} ${
+                isActive(path) ? styles.active : ""
+              }`}
+            >
+              {cat.name}
+            </Link>
+          );
+        })}
+      </div>
     </nav>
   );
 };
