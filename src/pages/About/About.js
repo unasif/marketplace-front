114d9da35a70dde7cd273a9c81cc705c1ccc9bdc
nazikhoma <128983249+nazikhoma@users.noreply.github.com>
2025-11-11 18:41:36 +0200

    Виправлення конфлікту після обєднання

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,62 +1,21 @@
 import styles from "./About.module.scss";
-import usePageModules from "../../hooks/usePageModules";
 
 export const About = () => {
-  const modules = usePageModules("about");
-
   return (
     <div className={styles.mainContainer}>
       <div className={styles.contentContainer}>
-        {modules.map((mod) => {
-          if (mod.module_type === "text") {
-            const content = mod.content || {};
-            const style = mod.styles || {};
-            return (
-              <div key={mod.id} style={style}>
-                {content.title && (
-                  <h3 style={{ fontWeight: "bold" }}>{content.title}</h3>
-                )}
-                {content.content && (
-                  <div
-                    dangerouslySetInnerHTML={{ __html: content.content }}
-                  />
-                )}
-              </div>
-            );
-          }
-          if (mod.module_type === "gallery") {
-            const { images = [] } = mod.content || {};
-            const { columns = 3 } = mod.styles || {};
-            return (
-              <div
-                key={mod.id}
-                style={{
-                  display: "grid",
-                  gridTemplateColumns: `repeat(${columns}, 1fr)`,
-                  gap: "20px",
-                  margin: "40px 0",
-                }}
-              >
-                {images.map((img, idx) => (
-                  <div key={idx} style={{ textAlign: "center" }}>
-                    <img
-                      src={img.url}
-                      alt={img.caption || ""}
-                      style={{ width: "100%", borderRadius: "8px" }}
-                    />
-                    {img.caption && (
-                      <div style={{ marginTop: "8px", color: "#555" }}>
-                        {img.caption}
-                      </div>
-                    )}
-                  </div>
-                ))}
-              </div>
-            );
-          }
-          // інші модулі добавлю потім
-          return null;
-        })}
+        <h3>Про нас</h3>
+        <p>
+          Існує багато варіацій уривків з Lorem Ipsum, але більшість з них
+          зазнала певних змін на кшталт жартівливих вставок або змішування слів,
+          які навіть не виглядають правдоподібно. Якщо ви збираєтесь
+          використовувати Lorem Ipsum, ви маєте упевнитись в тому, що всередині
+          тексту не приховано нічого, що могло б викликати у читача конфуз.
+          Більшість відомих генераторів Lorem Ipsum в Мережі генерують текст
+          шляхом повторення наперед заданих послідовностей Lorem Ipsum.
+          Принципова відмінність цього генератора робить його першим справжнім
+          генератором Lorem Ipsum.
+        </p>
       </div>
     </div>
   );
