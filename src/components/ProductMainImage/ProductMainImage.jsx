2965a8a46a347f1140445bad0fe9e0db3941f441
nazikhoma <128983249+nazikhoma@users.noreply.github.com>
2025-12-15 15:07:00 +0200

    оновлено відображення фотографій

--- src/components/ProductMainImage/ProductMainImage.jsx
+++ src/components/ProductMainImage/ProductMainImage.jsx
@@ -4,9 +4,16 @@ import noPhotoAvailable from "../../assets/no-photo-available.svg";
 const ProductMainImage = ({ product }) => {
   const { main_photo, name } = product;
   const baseURL = "https://unas.if.ua/api/";
-  const imageUrl = product.main_photo
-    ? `${baseURL}${product.main_photo.replace(/\\/g, "/")}`
-    : null;
+
+  let imageUrl = null;
+
+  if (main_photo) {
+    if (main_photo.startsWith("http") || main_photo.startsWith("https")) {
+      imageUrl = main_photo;
+    } else {
+      imageUrl = `${baseURL}${main_photo.replace(/\\/g, "/")}`;
+    }
+  }
 
   return (
     <div>
