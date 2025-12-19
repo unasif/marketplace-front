42d70abf4c6cbd73128e41b0b3d506234c07a0a7
VolodymyrPalahniuk <palagnyukvolodymyranatoliy@gmail.com>
2025-12-19 15:31:39 +0200

    Тест 6

--- src/components/CategoriesSidebar/CategoriesSidebar.jsx
+++ src/components/CategoriesSidebar/CategoriesSidebar.jsx
@@ -131,7 +131,7 @@ const CategoriesSidebar = () => {
                 const visibleLinks = group.links.slice(0, LINKS_LIMIT);
                 const hasMore = group.links.length > LINKS_LIMIT;
                 return (
-                  <Grid item xs={12} sm={6} md={3} key={index} zeroMinWidth>
+                  <Grid item xs={12} sm={6} md={3} key={index} style={{ minWidth: '250px' }}>
                     <div className={styles.groupBlock}>
                       
                       <h3 className={styles.groupTitle}>
