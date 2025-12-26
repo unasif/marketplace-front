6f753f7ae61eed23a8d8b5914d9bbd026a428272
VolodymyrPalahniuk <palagnyukvolodymyranatoliy@gmail.com>
2025-12-26 12:10:15 +0200

    Виправлено тінь

--- src/components/CategoriesSidebar/CategoriesSidebar.jsx
+++ src/components/CategoriesSidebar/CategoriesSidebar.jsx
@@ -121,7 +121,7 @@ const CategoriesSidebar = () => {
         {/* Права панель (Mega Menu) */}
         {activeCategory && (
           <Paper 
-          elevation={4} 
+          elevation={0}
           className={styles.flyoutPanel}
           >
             <h2 className={styles.flyoutTitle}>{activeCategory.name}</h2>
