6741611919bc844523a6f179c8a2b4e6507726e4
VolodymyrPalahniuk <palagnyukvolodymyranatoliy@gmail.com>
2026-01-08 14:17:50 +0200

    Перевірка логотипу інстаграму 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import { styled } from '@mui/material/styles';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
