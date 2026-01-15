9a0774880d705284d76ceaada897c868f2ac593b
VolodymyrPalahniuk <palagnyukvolodymyranatoliy@gmail.com>
2026-01-15 13:36:04 +0200

    Виправлення помилки

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from "./Header.module.scss";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import SearchProduct from "../SearchProduct/SearchProduct";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faHeart } from "@fortawesome/free-regular-svg-icons";
