c5ad912ea85ecb829d1b6366d7e2ffa3b0fafe94
nazikhoma <128983249+nazikhoma@users.noreply.github.com>
2026-01-20 17:38:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <StyledEngineProvider injectFirst>
-    <BrowserRouter>
+    <BrowserRouter basename="/marketdemo">
       <App />
     </BrowserRouter>
   </StyledEngineProvider>
