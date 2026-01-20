2b9fb1f49e985f81b8d80eb07974e143104ec8f7
nazikhoma <128983249+nazikhoma@users.noreply.github.com>
2026-01-20 14:02:29 +0200

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: '/api/', // Базова URL-адреса API
+  baseURL: '/marketdemo/api/', // Базова URL-адреса API
   headers: {
     'Content-Type': 'application/json', // Заголовки, які ви хочете передати
   },
