d56730e189196fae2f7b4e0b432cf21095e13c4d
VolodymyrPalahniuk <palagnyukvolodymyranatoliy@gmail.com>
2026-01-29 14:20:51 +0200

    Виправлення прошуку

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,21 +1,25 @@
 import { useState, useEffect } from "react";
 import { instance } from "../api";
 
-const useProducts = () => {
+const useProducts = (token) => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     // Функція для отримання списку товарів з використанням токену
     const fetchProducts = async () => {
       try {
-        const response = await instance.get("/product/");
+        const response = await instance.get("/product/", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
         setProducts(response.data);
       } catch (error) {
         console.error("Помилка отримання списку товарів:", error);
       }
     };
     fetchProducts();
-  }, []);
+  }, [token]);
 
   return products;
 };
