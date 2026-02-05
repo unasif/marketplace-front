2911cb0b7d2a68533ee393bb195d4117dfccba0f
nazikhoma <128983249+nazikhoma@users.noreply.github.com>
2026-02-05 11:00:01 +0200

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,5 @@
 import axios from 'axios';
 
-/*const getBaseUrl = () => {
-  if (window.location.pathname.startsWith('/marketdemo')) {
-      return '/marketdemo/api/';
-  }
-  return '/api/';
-};*/
-
 export const instance = axios.create({
   baseURL: "/api/", // Базова URL-адреса API
   headers: {
