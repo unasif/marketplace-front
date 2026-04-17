63bb6e2ea57d8ff790545bc0d569a860c0ba4f90
nazikhoma <128983249+nazikhoma@users.noreply.github.com>
2026-04-17 10:34:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,27 @@ import { BrowserRouter } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material/styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const getBasename = () => {
+  const path = window.location.pathname;
+  
+  if (path.startsWith('/testdevelopment')) {
+    return '/testdevelopment';
+  }
+  
+  if (path.startsWith('/marketdemo')) {
+    return '/marketdemo';
+  }
+  
+  return '/'; 
+};
+
+const basename = getBasename();
+
 root.render(
   // <React.StrictMode>
   <StyledEngineProvider injectFirst>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StyledEngineProvider>
